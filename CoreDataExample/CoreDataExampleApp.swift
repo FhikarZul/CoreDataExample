318f96350c3e713286d501ce704ed32f6614f7c4
Fhikar Zul <Fhikarzul18@gmail.com>
2022-08-30 12:15:54 +0800

    Initial Commit

---
+++ CoreDataExample/CoreDataExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataExampleApp.swift
+//  CoreDataExample
+//
+//  Created by Lucy on 30/08/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
